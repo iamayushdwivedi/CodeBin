e0850b0432fd51cdc0d75cb9d85dbc9efc3a9f73
iamayushdwivedi <84836231+iamayushdwivedi@users.noreply.github.com>
2021-05-26 12:59:29 +0530

    Add files via upload

---
+++ frequency_array.py
@@ -0,0 +1,22 @@
+t = int(input())
+while t > 0:
+    n = int(input())
+    arr = list(map(int, input().split()))
+    freq = [0] * 40
+    for i in range(n):
+        freq[arr[i]] += 1
+    '''for i in range(40):
+        if freq[i] > 0:
+            print(i, freq[i])'''
+
+    flag = 0
+    for i in range(40):
+        if freq[i] > 1:
+            flag = 1
+            break
+
+    if flag == 1:
+        print("Elements are not unique")
+
+    else:
+        print("Elements are unique")
